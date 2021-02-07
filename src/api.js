dab562fa2cf8802ea533ea9cb370097ec9cc49b8
imperiordragon <aayushs2797@gmail.com>
2021-02-07 19:08:11 +0530

    Added global styling and card component for game

--- src/api.js
+++ src/api.js
@@ -21,7 +21,7 @@ const getCurrentDay = () => {
     }
 };
 
-//Current day/month/year
+//format date in year/month/date
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
 const currentDay = getCurrentDay();
@@ -29,7 +29,7 @@ const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear-1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear+1}-${currentMonth}-${currentDay}`;
 
-//Popular games
+//Games Category
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
 const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
@@ -37,3 +37,7 @@ const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&pag
 export const popularGamesURL = () => `${BASE_URL}${popular_games}`;
 export const upcomingGamesURL = () => `${BASE_URL}${upcoming_games}`;
 export const newGamesURL = () => `${BASE_URL}${new_games}`;
+
+//Specific game details
+export const gameDetailsURL = (game_id) => `${BASE_URL}games/${game_id}`;
+export const gameScreenshotURL = (game_id) => `${BASE_URL}games/${game_id}/screenshots`;
